21e36be6b0d5755365e67969bb438651473ee09e
bujorgheorghe <56941435+bujorgheorghe@users.noreply.github.com>
2019-11-11 13:07:34 -0500

    Gradle setup done
    Gradle setup done

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -8,17 +8,22 @@ import org.testng.Assert;
 public class LoginTest {
 
     
-    public static void main(String[] args){
+    public static void main (String[] args){
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://deens-master.now.sh/");
+        driver.get("https://deens-master.now.sh/login");
+
         WebElement login_button = driver.findElement(By.cssSelector("[data-testid=\"loginSubmit\"]"));
         login_button.click();
-        WebElement uiErrorMessage =driver.findElement(By.cssSelector(".ui.error.message.p"));
-        String errorMessagetext = uiErrorMessage.getText();
-        Assert.assertEquals(errorMessagetext, "Empty email or password");
-        driver.quit();
 
+        WebElement uiErrorMessage = driver.findElement(By.cssSelector(".ui.error.message p"));
+
+        String errorMessagetext = uiErrorMessage.getText();
+
+        Assert.assertEquals(errorMessagetext, "Empty email or password");
+
+
+        driver.quit();
 
 
 
